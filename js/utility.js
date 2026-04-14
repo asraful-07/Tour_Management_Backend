d1a1bffbbf078d69f4bc7df6c8d31ab8d5354634
asraful-07 <asraful0508@gmail.com>
2026-04-14 18:14:44 +0600

    delete my old code

--- js/utility.js
+++
@@ -1,8 +0,0 @@
-//get value function
-function getInputValueById(id) {
-    return parseFloat(document.getElementById(id).value);
-};
-
-function getElementTextById(id){
-    return parseFloat(document.getElementById(id).innerText);
-};
